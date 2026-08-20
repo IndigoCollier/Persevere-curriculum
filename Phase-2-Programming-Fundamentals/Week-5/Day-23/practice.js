82d3f4251b865f3cbb305f58b1a89d31eb3aefb8
IndigoCollier <indigo.collier85@gmail.com>
2026-08-20 17:29:01 -0500

    Continue practing DOM manipulation with style changes, live text input and Max.math

--- Phase-2-Programming-Fundamentals/Week-5/Day-23/practice.js
+++ Phase-2-Programming-Fundamentals/Week-5/Day-23/practice.js
@@ -8,11 +8,74 @@ greetBtn.addEventListener('click', function() {
   greetBtn.style.backgroundColor = 'pink';
   greetBtn.textContent = 'Revert Back';
   body.style.backgroundColor = 'pink';
-    } else //(greetBtn.textContent = 'Revert Back' )
+    } else 
      {
         greetBtn.textContent = 'Change Greeting';
         greeting.textContent = 'Im back!';
-        body.style.backgroundColor = 'green';
+        body.style.backgroundColor = 'white';
        
     }
+}) 
+
+const userInput = document.querySelector('#user-input');
+const output = document.querySelector('#output');
+
+userInput.addEventListener('input', function(){
+  if (userInput.value === '') {
+    output.textContent = 'Please type something first.';
+  } else {
+  output.textContent = userInput.value; 
+  }
+});
+
+let guest = 0;
+const counter = document.querySelector('#count');
+const countUp = document.querySelector('#count-up');
+const removeGuest = document.querySelector('#remove-guest');
+const counterMsg = document.querySelector('#counter-message');
+
+countUp.addEventListener('click', function(){
+  if (guest < 10) {
+     guest++;
+  counter.textContent = guest;
+  } 
+  if (guest === 10) {
+    counterMsg.textContent = 'Maximum Capacity Reached!';
+  }
 })
+
+removeGuest.addEventListener('click', function(){
+  guest = Math.max(0, guest - 1);
+  counter.textContent = guest;
+  if (guest < 10) {
+  counterMsg.textContent = '';
+  }
+});
+
+const redBtn = document.querySelector('#red-btn');
+const blueBtn = document.querySelector('#blue-btn');
+const greenBtn = document.querySelector('#green-btn');
+const resetBtn = document.querySelector('#reset-btn');
+const colorLabel = document.querySelector('#color-label');
+const colorBox = document.querySelector('#color-box');
+
+redBtn.addEventListener('click', function() {
+  colorLabel.textContent = 'Red button selected.';
+  colorBox.style.backgroundColor = 'red';
+});
+
+blueBtn.addEventListener('click', function() {
+  colorLabel.textContent = 'Blue button selected.';
+  colorBox.style.backgroundColor = 'blue';
+});
+
+greenBtn.addEventListener('click', function() {
+  colorLabel.textContent = 'Green button selected.';
+  colorBox.style.backgroundColor = 'green';
+});
+
+resetBtn.addEventListener('click', function() {
+  colorLabel.textContent = 'Pick a color.'
+  colorBox.style.backgroundColor = 'lightgrey'
+});
+
